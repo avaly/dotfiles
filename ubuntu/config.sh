8b4061f5f70b4d5d8227e6627ebdb91d2c5330e2
Valentin Agachi <github-com@agachi.name>
2014-05-01 11:55:30 +0200

    Fix config script for filenames with spaces

--- ubuntu/config.sh
+++ ubuntu/config.sh
@@ -10,30 +10,33 @@
 # Install config files for various applications
 #
 init_config_files() {
-
 	CONFIG=$HOME/.dotfiles/ubuntu/config
+
+	OLDIFS=$IFS
+	IFS=$(echo -en "\n\b")
 
 	for FILE in $(find $CONFIG -type f -print); do
 		FILE=${FILE#$CONFIG/}
-		if [ -h $HOME/$FILE ]; then
+		if [ -h "$HOME/$FILE" ]; then
 			echo "Replacing existing symlink $HOME/$FILE"
-			unlink $HOME/$FILE
+			unlink "$HOME/$FILE"
 		else
-			if [ -f $HOME/$FILE ]; then
+			if [ -f "$HOME/$FILE" ]; then
 				echo "Replacing existing file $HOME/$FILE"
-				rm $HOME/$FILE
+				rm "$HOME/$FILE"
 			else
 				echo "Symlinking $HOME/$FILE"
 			fi
 		fi
-		DIR=$(dirname $HOME/$FILE)
-		if [ ! -d $DIR ]; then
+		DIR=$(dirname "$HOME/$FILE")
+		if [ ! -d "$DIR" ]; then
 			echo "Creating directory $DIR"
-			mkdir -p $DIR
+			mkdir -p "$DIR"
 		fi
-		ln -s $CONFIG/$FILE $HOME/$FILE
+		ln -s "$CONFIG/$FILE" "$HOME/$FILE"
 	done
 
+	IFS=$OLDIFS
 }
 
 
