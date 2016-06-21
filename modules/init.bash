84f578a4992e1d00386551c6791e718f1760b7fa
Valentin Agachi <github-com@agachi.name>
2016-06-21 18:45:03 +0100

    Fix init syntax

--- modules/init.bash
+++ modules/init.bash
@@ -1,10 +1,10 @@
 # Mount external drives
 
-if [ $(iwgetid -r) == "avaly" ]; then
+if [[ $(iwgetid -r) == "avaly" ]]; then
 	$(sudo mount -l | grep /media/nas > /dev/null 2>&1)
 	NAS_MOUNTED=$?
 
-	if [ "$NAS_MOUNTED" == "1" ]; then
+	if [[ "$NAS_MOUNTED" == "1" ]]; then
 		sudo mount -a
 	fi
 fi
