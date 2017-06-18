c41db9e072e8483235f6fac9d31f82e7de8f14f5
Valentin Agachi <github-com@agachi.name>
2017-06-18 22:34:40 +0200

    git prompt updates based on official git prompt

--- modules/prompt.bash
+++ modules/prompt.bash
@@ -5,63 +5,97 @@
 # - liquidprompt
 # - https://github.com/git/git/blob/master/contrib/completion/git-prompt.sh
 
-_prompt_git_branch()
-{
+function _prompt_git_branch() {
   local branch="$(git symbolic-ref HEAD 2>/dev/null)"
   if [[ $? -ne 0 || -z "$branch" ]] ; then
-    # In detached head state, use describe instead
-    branch="$(git describe HEAD 2>/dev/null)"
+  # In detached head state, use describe instead
+  branch="$(git describe HEAD 2>/dev/null)"
   fi
   [[ $? -ne 0 || -z "$branch" ]] && return
   branch="${branch#refs/heads/}"
   echo $(printf "%q" "$branch")
 }
 
-_prompt_git_status()
-{
-  local branch
-  branch=$(_prompt_git_branch)
-  if [[ -z "$branch" ]] ; then
+function __git_read() {
+  local f="$1"
+  shift
+  test -r "$f" && read "$@" <"$f"
+}
+
+function _prompt_git_status() {
+  local branch=$(_prompt_git_branch)
+  if [[ -z "$branch" ]]; then
     return
   fi
 
-  local GD
-  git diff --quiet >/dev/null 2>&1
-  GD=$?
+  local git_dir=".git"
 
-  local GDC
+  local action=""
+  if [ -d "$git_dir/rebase-merge" ]; then
+    __git_read "$git_dir/rebase-merge/head-name" rebase_branch
+    __git_read "$git_dir/rebase-merge/msgnum" step
+    __git_read "$git_dir/rebase-merge/end" total
+    if [ -f "$git_dir/rebase-merge/interactive" ]; then
+      action="${Magenta} REBASE-i"
+    else
+      action="${Magenta} REBASE-m"
+    fi
+  else
+    if [ -d "$g/rebase-apply" ]; then
+      __git_read "$g/rebase-apply/next" step
+      __git_read "$g/rebase-apply/last" total
+      if [ -f "$g/rebase-apply/rebasing" ]; then
+        action="${Magenta} REBASE"
+      elif [ -f "$g/rebase-apply/applying" ]; then
+        action="${Magenta} AM"
+      else
+        action="${Magenta} AM/REBASE"
+      fi
+    elif [ -f "$g/MERGE_HEAD" ]; then
+      action="${Magenta} MERGING"
+    elif [ -f "$g/CHERRY_PICK_HEAD" ]; then
+      action="${Magenta} CHERRY-PICKING"
+    elif [ -f "$g/REVERT_HEAD" ]; then
+      action="${Magenta} REVERTING"
+    elif [ -f "$g/BISECT_LOG" ]; then
+      action="${Magenta} BISECTING"
+    fi
+  fi
+
+  if [ -n "$step" ] && [ -n "$total" ]; then
+    action="${action} ${step}/${total}"
+  fi
+
+  git diff --quiet >/dev/null 2>&1
+  local diff_result=$?
+
   git diff --cached --quiet >/dev/null 2>&1
-  GDC=$?
+  local diff_cached_result=$?
 
   local has_staged
-  if [[ "$GDC" -eq "1" ]] ; then
+  if [[ "$diff_cached_result" -eq "1" ]] ; then
     has_staged=" ${Yellow}staged"
   fi
 
-  local has_untracked
-  has_untracked=$(git ls-files --others --exclude-standard --directory --no-empty-directory --error-unmatch -- ':/*' 2>/dev/null)
-  if [[ -z "$has_untracked" ]] ; then
-    has_untracked=""
-  else
+  local untracked=$(git ls-files --others --exclude-standard --directory \
+    --no-empty-directory --error-unmatch -- ':/*' 2>/dev/null)
+  local has_untracked=""
+  if [[ ! -z "$untracked" ]] ; then
     has_untracked=" ${Yellow}untracked"
   fi
 
-  local has_stash
-  has_stash=$(git stash list 2>/dev/null)
-  if [[ -z "$has_stash" ]] ; then
-    has_stash=""
-  else
+  local stash=$(git stash list 2>/dev/null)
+  local has_stash=""
+  if [[ ! -z "$has_stash" ]] ; then
     has_stash=" ${BrightYellow}stash"
   fi
 
-  local remote
-  remote="$(git config --get branch.${branch}.remote 2>/dev/null)"
+  local remote="$(git config --get branch.${branch}.remote 2>/dev/null)"
   # if git has no upstream, use origin
   if [[ -z "$remote" ]]; then
     remote="origin"
   fi
-  local remote_branch
-  remote_branch="$(git config --get branch.${branch}.merge 2>/dev/null)"
+  local remote_branch="$(git config --get branch.${branch}.merge 2>/dev/null)"
   # without any remote branch, use the same name
   if [[ -z "$remote_branch" ]]; then
     remote_branch="$branch"
@@ -69,31 +103,31 @@ _prompt_git_status()
     remote_branch="${remote_branch#refs/heads/}"
   fi
 
-  local commits
-  commits=0
+  local commits=0
   if [[ -n "$remote" && -n "$remote_branch" ]] ; then
-    commits=$(git rev-list --no-merges --count ${remote}/${remote_branch}..${branch} 2>/dev/null)
+    commits=$(git rev-list --no-merges --count \
+      ${remote}/${remote_branch}..${branch} 2>/dev/null)
     if [[ -z "$commits" ]] ; then
       commits=0
     fi
   fi
 
-  local has_commit
+  local has_commit=""
   if [[ "$commits" -gt "0" ]] ; then
     has_commit="${Blue} ${commits} behind remote"
   fi
 
-  local result
-  if [[ "$GD" -eq "1" ]] ; then
+  local result=""
+  if [[ "$diff_result" -eq "1" ]] ; then
     # unstaged changes
-    result="${BrightRed}${branch}${has_staged}${has_untracked}${has_commit}${has_stash}"
+    result="${BrightRed}${branch}${has_staged}${has_untracked}${has_commit}${has_stash}${action}"
   else
-    if [[ "$GDC" -eq "1" ]] ; then
+    if [[ "$diff_cached_result" -eq "1" ]] ; then
       # staged changes
-      result="${Magenta}${branch}${has_staged}${has_untracked}${has_commit}${has_stash}"
+      result="${Magenta}${branch}${has_staged}${has_untracked}${has_commit}${has_stash}${action}"
     else
       # clean
-      result="${Cyan}${branch}${has_staged}${has_untracked}${has_commit}${has_stash}"
+      result="${Cyan}${branch}${has_staged}${has_untracked}${has_commit}${has_stash}${action}"
     fi
   fi
 
