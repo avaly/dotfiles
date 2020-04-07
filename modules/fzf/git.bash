c472adbc5739310c6b5c81f74dace6a16fc260ba
Valentin Agachi <github-com@agachi.name>
2020-04-07 15:51:17 +0100

    git aliases

--- modules/fzf/git.bash
+++ modules/fzf/git.bash
@@ -22,23 +22,21 @@ gf() {
 
 gb() {
   is_in_git_repo || return
-  git branch -a --color=always | grep -v '/HEAD\s' | sort |
-  fzf-down --ansi --multi --tac --preview-window right:70% \
-    --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1) | head -'$LINES |
-  sed 's/^..//' | cut -d' ' -f1 |
-  sed 's#^remotes/##'
+  git for-each-ref --sort=-committerdate --format='%(refname:short)' refs/heads/ |
+  fzf-down --ansi --multi --preview-window "right:70%" \
+    --preview 'git l --color=always $(echo {} | cut -d" " -f1) | head -'$LINES
 }
 
 gt() {
   is_in_git_repo || return
   git tag --sort -version:refname |
-  fzf-down --multi --preview-window right:70% \
+  fzf-down --multi --preview-window "right:70%" \
     --preview 'git show --color=always {} | head -'$LINES
 }
 
-gh() {
+gc() {
   is_in_git_repo || return
-  git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
+  git l --color=always |
   fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
     --header 'Press CTRL-S to toggle sort' \
     --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | head -'$LINES |
@@ -51,6 +49,16 @@ gr() {
   fzf-down --tac \
     --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" {1} | head -200' |
   cut -d$'\t' -f1
+}
+
+gbr() {
+  is_in_git_repo || return
+  git branch -D $(gb)
+}
+
+gcs() {
+  is_in_git_repo || return
+  git show $(gc)
 }
 
 bind '"\er": redraw-current-line'
