4dd8a568d14c04bd7c466d71be0d4f4c49186114
Valentin Agachi <github-com@agachi.name>
2021-01-10 18:45:03 +0100

    Remove mount

--- modules/init.bash
+++ modules/init.bash
@@ -1,14 +1,3 @@
-# Mount external drives
-
-if [[ $(iwgetid -r) == "$WIFI_LAN_ID" ]]; then
-	$(sudo mount -l | grep /media/nas > /dev/null 2>&1)
-	NAS_MOUNTED=$?
-
-	if [[ "$NAS_MOUNTED" == "1" ]]; then
-		sudo mount -a
-	fi
-fi
-
 # z
 source ~/apps/z/z.sh
 
